e3cae5de770f272c5f7a3a2aa5819c6fabdb52d0
Kaik Aciole <kaikaciole1.0@gmail.com>
2025-02-24 13:01:07 -0300

    feat: adicionando classe de conexão ao mongo e estruturando pacotes

---
+++ src/main/java/br/com/crudmongo/utils/ConnectMongoDB.java
@@ -0,0 +1,19 @@
+package br.com.crudmongo;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+
+public class ConnectMongoDB {
+
+    public static void getConnection(String uri, String nameDB) {
+
+        try(MongoClient cliente = MongoClients.create(uri)) {
+            MongoDatabase banco = cliente.getDatabase(nameDB);
+            System.out.println("Conectado ao banco: " + banco.getName());
+        } catch (Exception e) {
+            System.err.println("Erro ao conectar: " + e.getMessage());
+        }
+    }
+
+}
